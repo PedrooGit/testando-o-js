e0bc3e44f73b8f62f7bd1590d9d31847a73cae12
PedrooGit <pedroogit@gmail.com>
2026-05-03 17:43:29 -0300

    estudei jv, como trocar tipos de variaveis

---
+++ index_1.js
@@ -0,0 +1,8 @@
+var nome = prompt(`qual seu nome?`);
+document.write(`<h2> ${nome} o seu tem ${nome.length} tantas letras. </h2>`);
+
+var n1 = prompt("Fale um numero");
+var n2 = prompt("Fale outro");
+var s = Number(n1) + Number(n2);
+
+alert(String(s));
