646d8b41b8b4d522856fb54d780dcfe6b60530b4
PedrooGit <pedroogit@gmail.com>
2026-05-03 19:02:26 -0300

    DOM como pegar do hmtl

---
+++ index_2.js
@@ -0,0 +1,13 @@
+var p1 = document.getElementsByTagName('p')[0];
+var id = document.getElementById("hello");
+var clas = document.getElementsByClassName(`opa`)[0];
+
+
+document.write(`<h4>esta pegando:</h4>${clas.innerHTML}`);
+
+
+
+
+
+
+
