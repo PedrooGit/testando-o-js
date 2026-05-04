a4a16fcfbbf05b1b291b1a04f8a3eea64323d3d2
PedrooGit <pedroogit@gmail.com>
2026-05-04 20:00:15 -0300

    sobre manipular DOM, por cima só com mouse

---
+++ index_3.js
@@ -0,0 +1,20 @@
+    var a = document.getElementById('area');
+
+function eta(){
+    a.innerText = "Voce clickou";
+    a.style.background = 'yellow'    
+}
+
+function opa(){
+    a.innerText = "Entrou";
+    a.style.background = 'forestgreen'
+}
+
+function eba(){
+    a.innerText = "Saiu";
+    a.style.background = 'red'
+}
+
+
+
+
